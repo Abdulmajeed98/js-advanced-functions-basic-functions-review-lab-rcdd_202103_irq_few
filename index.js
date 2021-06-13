6b3acc03b9a1fe5991efc40e0ab8d0015b5cb0dc
Abdulmajeed Jaafer <abdulmajeedjaafer76@gmail.com>
2021-06-13 12:54:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function saturdayFun(activity="roller-skate") {
   return `This Saturday, I want to ${activity}!`
 }
 
-let mondayWork = function(activity="go to the office") {
+const mondayWork = function(activity="go to the office") {
   return `This Monday, I will ${activity}.`
 }
 
-let wrapAdjective = function(style="*") {
+const wrapAdjective = function(style="*") {
   return function(adjective="special") {
     return `You are ${style}${adjective}${style}!`
   }
